c0ef538a74a7852383881e196a0a7a21e1cfb85f
VasileiosDiamantidis <vasileios.diamantidis@zattoo.com>
2021-02-17 11:02:13 +0100

    Initial Commit

---
+++ PlayerIsReadyForDisplayIssue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayerIsReadyForDisplayIssue
+//
+//  Created by Vasileios on 17.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
