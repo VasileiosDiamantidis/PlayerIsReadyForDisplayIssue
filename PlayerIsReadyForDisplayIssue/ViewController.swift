21aceca130df3ad61c3684fe3b38a7ad6b8242a9
David Cordero <david@corderoramirez.com>
2021-02-17 14:15:22 +0100

    Add extra logging

--- PlayerIsReadyForDisplayIssue/ViewController.swift
+++ PlayerIsReadyForDisplayIssue/ViewController.swift
@@ -10,6 +10,10 @@ import AVKit
 import AVFoundation
 import TvOSSlider
 
+private var playerItemStatusContext = 0
+private var playerContext = 1
+private var playerStatusContext = 2
+
 class ViewController: UIViewController {
     
     /* normal stream play URL*/
@@ -24,11 +28,8 @@ class ViewController: UIViewController {
     private var slider: TvOSSlider!
     private var valueLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-    }
+    private var player: AVPlayer!
+    private var playerItem: AVPlayerItem!
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -40,8 +41,16 @@ class ViewController: UIViewController {
     
     private func setUpPlayer(link: String) {
         if let playerURL = URL.init(string: link) {
-            let avplayer = AVPlayer(playerItem: AVPlayerItem(url: playerURL))
-            previewLayer = AVPlayerLayer(player: avplayer)
+            playerItem = AVPlayerItem(url: playerURL)
+            
+            playerItem.addObserver(self, forKeyPath: #keyPath(AVPlayerItem.status), options: [.old, .new], context: &playerItemStatusContext)
+    
+            player = AVPlayer(playerItem: playerItem)
+            
+            player.addObserver(self, forKeyPath: #keyPath(AVPlayer.rate), options: [.initial, .old, .new], context: &playerContext)
+            player.addObserver(self, forKeyPath: #keyPath(AVPlayer.status), options: [.initial, .old, .new], context: &playerStatusContext)
+
+            previewLayer = AVPlayerLayer(player: player)
             previewLayer?.frame = self.view.frame
             self.view.layer.addSublayer(previewLayer!)
         }
@@ -75,5 +84,71 @@ class ViewController: UIViewController {
         valueLabel.text = "Will seek to \(date)"
     }
     
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        guard context == &playerItemStatusContext || context == &playerContext || context == &playerStatusContext else {
+            return super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+        
+        if context == &playerItemStatusContext {
+            if keyPath == #keyPath(AVPlayerItem.status) {
+                let status: AVPlayerItem.Status
+                
+                // Get the status change from the change dictionary
+                if let statusNumber = change?[.newKey] as? NSNumber {
+                    status = AVPlayerItem.Status(rawValue: statusNumber.intValue)!
+                } else {
+                    status = .unknown
+                }
+                
+                // Switch over the status
+                switch status {
+                case .readyToPlay:
+                    print("Player item is ready to play.")
+                case .failed:
+                    print("Player item failed. See error.")
+                case .unknown:
+                    print("Player item is not yet ready.")
+                @unknown default:
+                    ()
+                }
+            }
+        }
+        else if context == &playerContext {
+            if keyPath == #keyPath(AVPlayer.rate) {
+                let rate = change?[.newKey] as? Float
+                
+                if let rate = rate {
+                    print("Player, rate changed to: \(rate)")
+                }
+                else {
+                    print("Player, no rate found")
+                }
+            }
+        }
+        else if context == &playerStatusContext {
+            if keyPath == #keyPath(AVPlayer.status) {
+                let status: AVPlayer.Status
+                
+                // Get the status change from the change dictionary
+                if let statusNumber = change?[.newKey] as? NSNumber {
+                    status = AVPlayer.Status(rawValue: statusNumber.intValue)!
+                } else {
+                    status = .unknown
+                }
+                
+                // Switch over the status
+                switch status {
+                case .readyToPlay:
+                    print("Player is ready to play")
+                case .failed:
+                    print("Player failed. See error.")
+                case .unknown:
+                    print("Player is not yet ready.")
+                @unknown default:
+                    ()
+                }
+            }
+        }
+    }
 }
 
